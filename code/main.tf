62d7d599ed74936a89e5d58f8afe78853cb3a86b
Ishan Mahajan <mahanishanmahajan@gmail.com>
2024-07-04 14:02:39 +0100

    Implementing Data keyword for image

--- code/main.tf
+++ code/main.tf
@@ -2,6 +2,12 @@ resource "google_compute_address" "compute_external_ip" {
   name = "myip"
 }
 
+data "google_compute_image" "ubuntu_image" {
+  family  = "ubuntu-2004-lts-arm64"
+  project = "ubuntu-os-cloud"
+}
+
+
 resource "google_compute_instance" "compute" {
   name         = "compute01"
   machine_type = "e2-standard-2"
@@ -9,7 +15,7 @@ resource "google_compute_instance" "compute" {
   tags = ["testing"]
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts-arm64"
+      image = data.google_compute_image.ubuntu_image.self_link
       labels = {
         my_label = "value"
       }

e227876269e51f70803d570ed19df884c2485b7b
Ishan Mahajan <mahanishanmahajan@gmail.com>
2024-07-07 12:53:21 +0100

    Implementing variables in the code

--- code/provider.tf
+++ code/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = file("../learn-426012-199996f158be.json")
-    project = "learn-426012"
-    region = "us-central1"
+    credentials = file(var.gcp_key)
+    project = var.gcp_project
+    region = var.gcp_region
 }

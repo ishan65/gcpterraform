ce52c28932b983accb1106a758c09368b1a4581f
Ishan Mahajan <mahanishanmahajan@gmail.com>
2024-07-07 16:53:39 +0100

    Implementing count and variables

--- code/main.tf
+++ code/main.tf
@@ -1,21 +1,29 @@
-resource "google_compute_address" "compute_external_ip" {
-  name = "myip"
-}
-
 data "google_compute_image" "ubuntu_image" {
-  family  = "ubuntu-2004-lts"
+   family  = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
 }
+
+ data "google_compute_zones" "zone_info" {
+}
+
 
 resource "google_compute_network" "vpc_network01" {
   name                    = "vpc-network01"
   auto_create_subnetworks = false
 }
 
+resource "google_compute_address" "compute_external_ip" {
+  count = length(data.google_compute_zones.zone_info.names)
+  name = "myip-${data.google_compute_zones.zone_info.names[count.index]}"
+}
+
+
 resource "google_compute_subnetwork" "network-with-private-ip-ranges" {
-  name          = "us-central1-subnetwork"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
+  count = length(data.google_compute_zones.zone_info.names)
+
+  name          = "${data.google_compute_zones.zone_info.names[count.index]}-subnetwork"
+  ip_cidr_range = "10.0.${count.index}.0/24"
+  region        = var.gcp_region[0]
   network       = google_compute_network.vpc_network01.id
 }
 
@@ -36,9 +44,11 @@ resource "google_compute_firewall" "firewall01" {
 }
 
 resource "google_compute_instance" "compute01" {
-  name         = "compute01"
+  count = length(data.google_compute_zones.zone_info.names)
+
+  name         = "compute0${count.index}-${data.google_compute_zones.zone_info.names[count.index]}"
   machine_type = "e2-standard-2"
-  zone         = "us-central1-a"
+  zone         = "${data.google_compute_zones.zone_info.names[count.index]}"
   tags         = ["compute"]
 
   boot_disk {
@@ -49,10 +59,10 @@ resource "google_compute_instance" "compute01" {
 
   network_interface {
     network    = google_compute_network.vpc_network01.self_link
-    subnetwork = google_compute_subnetwork.network-with-private-ip-ranges.self_link
+    subnetwork = "${google_compute_subnetwork.network-with-private-ip-ranges[count.index].self_link}"
 
     access_config {
-      nat_ip = google_compute_address.compute_external_ip.address
+      nat_ip = "${google_compute_address.compute_external_ip[count.index].address}"
     }
   }
 
@@ -70,5 +80,11 @@ resource "google_compute_project_metadata" "gcp_ssh_key" {
 
 
 output "external_ip" {
-  value = google_compute_address.compute_external_ip.address
+  value = google_compute_address.compute_external_ip[*].address
 }
+
+
+output "zoneinfo" {
+  value = data.google_compute_zones.zone_info.names
+}
+
